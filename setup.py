2e630e0ea1d2973ad78c927c803db4d84e3d06db
rubenafo <rbfrancos@gmail.com>
2021-03-05 23:25:17 +0000

    Add version 0.9.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="krakee",
-    version="0.9.1",
+    version="0.9.2",
     author="Ruben Afonso",
     author_email="rbfrancos@gmail.com",
     description="Kraken Exchange API with a thin pandas Dataframe wrap",
